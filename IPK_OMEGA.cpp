7887efb221bdcb7c650a670dd6f1bc2ac389a057
xnovik03 <147273877+xnovik03@users.noreply.github.com>
2025-03-13 21:57:19 +0100

    Update IPK_OMEGA.cpp

--- IPK_OMEGA.cpp
+++ IPK_OMEGA.cpp
@@ -6,7 +6,10 @@
 #include <cstring>   // pro strncpy
 #include "NetworkInterface.h"
 #include "PortRangeParser.h"
+#include "TCPscanner.h"
+#include "UDPscanner.h"
 
+// Validace argumentů
 bool validateArguments(int argc, char* argv[]) {
     if (argc < 3) {
         std::cout << "ERROR: Invalid number of arguments. Usage: ./ipk-l4-scan -i <interface> -t <port>" << std::endl;
@@ -15,13 +18,9 @@ bool validateArguments(int argc, char* argv[]) {
     return true;
 }
 
-void processArguments(int argc, char* argv[]) {
-    std::vector<std::string> interfaces;
+// Zpracování argumentů z příkazové řádky
+void processArguments(int argc, char* argv[], std::string &interfaceName, std::string &tcpPorts, std::string &udpPorts, int &timeout) {
     bool interfaceProvided = false;
-    std::string interfaceName;
-    std::string portRange;  // Proměnná pro uchování portů
-    int timeout = 5000;
-
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-i" || arg == "--interface") {
@@ -38,15 +37,15 @@ void processArguments(int argc, char* argv[]) {
         }
         else if (arg == "--pt" || arg == "-t") {
             if (i + 1 < argc) {
-                portRange = argv[i + 1];  // Uložení portů do proměnné
-                std::cout << "TCP Ports: " << portRange << std::endl;
+                tcpPorts = argv[i + 1];
+                std::cout << "TCP Ports: " << tcpPorts << std::endl;
                 i++;
             }
         }
         else if (arg == "--pu" || arg == "-u") {
             if (i + 1 < argc) {
-                portRange = argv[i + 1];  // Uložení UDP portů
-                std::cout << "UDP Ports: " << portRange << std::endl;
+                udpPorts = argv[i + 1];
+                std::cout << "UDP Ports: " << udpPorts << std::endl;
                 i++;
             }
         }
@@ -58,33 +57,34 @@ void processArguments(int argc, char* argv[]) {
             }
         }
     }
-
-    // Pokud nebyl zadaný interface, vybíráme první dostupný
     if (!interfaceProvided) {
-        interfaces = NetworkInterface::getAvailableInterfaces();
-        if (interfaces.empty()) {
-            std::cout << "No network interfaces found!" << std::endl;
-            return;
-        }
-        NetworkInterface::selectInterface(interfaces);
-    }
-
-    // Zpracování portRange pro TCP/UDP
-    if (!portRange.empty()) {
-        std::vector<int> ports = PortRangeParser::parsePortRanges(portRange);
-        std::cout << "Selected Ports: ";
-        for (int port : ports) {
-            std::cout << port << " ";
-        }
-        std::cout << std::endl;
+        std::cout << "ERROR: Network interface not provided" << std::endl;
     }
 }
 
+// Hlavní funkce
 int main(int argc, char* argv[]) {
     if (!validateArguments(argc, argv)) {
         return 1;
     }
 
-    processArguments(argc, argv);
+    std::string interfaceName, tcpPorts, udpPorts;
+    int timeout = 5000;
+
+    // Zpracování argumentů
+    processArguments(argc, argv, interfaceName, tcpPorts, udpPorts, timeout);
+
+    // Převod TCP portů na čísla
+    std::vector<int> tcpPortNumbers = PortRangeParser::parsePortRanges(tcpPorts);
+    for (int port : tcpPortNumbers) {
+        TCPScanner::scanPort("localhost", port);  // Místo localhost použijte IP
+    }
+
+    // Převod UDP portů na čísla
+    std::vector<int> udpPortNumbers = PortRangeParser::parsePortRanges(udpPorts);
+    for (int port : udpPortNumbers) {
+        UDPScanner::scanPort("localhost", port);  // Místo localhost použijte IP
+    }
+
     return 0;
 }
