377f4f50828c086a7a0da6fea938ec542f592356
xnovik03 <147273877+xnovik03@users.noreply.github.com>
2025-03-13 21:56:08 +0100

    Update TCPscanner.cpp

--- TCPscanner.cpp
+++ TCPscanner.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
 #include "TCPscanner.h"
+
+// Implementace metody scanPort
+void TCPScanner::scanPort(const std::string &ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        std::cout << "Socket creation failed." << std::endl;
+        return;
+    }
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    // Pokus o připojení na port
+    int result = connect(sockfd, (struct sockaddr *)&server, sizeof(server));
+    
+    if (result == -1) {
+        std::cout << ip << " " << port << " tcp closed" << std::endl;
+    } else {
+        std::cout << ip << " " << port << " tcp open" << std::endl;
+    }
+
+    close(sockfd);
+}
