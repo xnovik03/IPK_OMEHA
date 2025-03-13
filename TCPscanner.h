d4dcd052760dde9c1b96ab899102419687b84480
xnovik03 <147273877+xnovik03@users.noreply.github.com>
2025-03-13 21:56:36 +0100

    Update TCPscanner.h

--- TCPscanner.h
+++ TCPscanner.h
@@ -1,5 +1,13 @@
-#pragma once
-class TCPscanner
-{
+#ifndef TCPSCANNER_H
+#define TCPSCANNER_H
+
+#include <string>
+#include <vector>
+
+class TCPScanner {
+public:
+    // Funkce pro skenování jednotlivého portu
+    static void scanPort(const std::string &ip, int port);
 };
 
+#endif // TCPSCANNER_H
