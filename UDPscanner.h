285d1c996234fe5f7002d8ae326cce7ddf5272bc
xnovik03 <147273877+xnovik03@users.noreply.github.com>
2025-03-13 21:55:23 +0100

    Update UDPscanner.h

--- UDPscanner.h
+++ UDPscanner.h
@@ -1,5 +1,12 @@
-#pragma once
-class UDPscanner
-{
+#ifndef UDPSCANNER_H
+#define UDPSCANNER_H
+
+#include <string>
+#include <vector>
+
+class UDPScanner {
+public:
+    static void scanPort(const std::string &ip, int port);  // Deklarace metody
 };
 
+#endif // UDPSCANNER_H
