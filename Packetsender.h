efd13816b5728e837030703c248cbcd0c17be7ec
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 15:48:03 +0100

    Aktualizovat Packetsender.h
    add processing of sending packages

--- Packetsender.h
+++ Packetsender.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <string>
+#include <netinet/in.h>
+
 class Packetsender
 {
-};
+public:
+    // Metoda pro odeslání SYN paketu pro TCP skenování
+    static bool sendTcpPacket(const std::string &ip, int port);
 
+    // Metoda pro odeslání UDP paketu
+    static bool sendUdpPacket(const std::string &ip, int port);
+};
