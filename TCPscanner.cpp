7a54bca367b107da85c512cc9f515ff81916e47f
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 15:50:29 +0100

    add processing of sending packages

--- TCPscanner.cpp
+++ TCPscanner.cpp
@@ -7,28 +7,16 @@
 #include <unistd.h>
 #include <netdb.h>
 #include "TCPscanner.h"
+#include "Packetsender.h"
 
 // Implementace metody scanPort
 void TCPScanner::scanPort(const std::string &ip, int port) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        std::cout << "Socket creation failed." << std::endl;
-        return;
-    }
+    // Místo implementace odesílání paketů voláme Packetsender
+    bool result = Packetsender::sendTcpPacket(ip, port);  // Odeslání TCP SYN paketu
 
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(ip.c_str());
-
-    // Pokus o připojení na port
-    int result = connect(sockfd, (struct sockaddr *)&server, sizeof(server));
-    
-    if (result == -1) {
-        std::cout << ip << " " << port << " tcp closed" << std::endl;
-    } else {
+    if (result) {
         std::cout << ip << " " << port << " tcp open" << std::endl;
+    } else {
+        std::cout << ip << " " << port << " tcp closed" << std::endl;
     }
-
-    close(sockfd);
 }
