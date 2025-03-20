856928a6790be6d4815d23a14bc8218d243f65e6
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 16:13:16 +0100

    add result format

--- ResultFormatter.h
+++ ResultFormatter.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <string>
+#include <iostream>
+
 class ResultFormatter
 {
-};
+public:
+    // Formátování výsledků pro TCP porty
+    static void formatTcpResult(const std::string &ip, int port, const std::string &status);
 
+    // Formátování výsledků pro UDP porty
+    static void formatUdpResult(const std::string &ip, int port, const std::string &status);
+};
