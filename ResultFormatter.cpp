edcc5746a5b19c44ca3321ec409d564a7df2eff3
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 16:12:20 +0100

    add result format

--- ResultFormatter.cpp
+++ ResultFormatter.cpp
@@ -1,1 +1,16 @@
 #include "ResultFormatter.h"
+#include <iostream>
+
+// Implementace formátování výsledků pro TCP
+void ResultFormatter::formatTcpResult(const std::string &ip, int port, const std::string &status)
+{
+    // Výstup formátovaný pro TCP
+    std::cout << ip << " " << port << " tcp " << status << std::endl;
+}
+
+// Implementace formátování výsledků pro UDP
+void ResultFormatter::formatUdpResult(const std::string &ip, int port, const std::string &status)
+{
+    // Výstup formátovaný pro UDP
+    std::cout << ip << " " << port << " udp " << status << std::endl;
+}
