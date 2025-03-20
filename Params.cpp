5a2b9dc2df1e69ac2238fe725c385fe8fe7d3ae2
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 17:02:07 +0100

    add parameter centralization

--- Params.cpp
+++ Params.cpp
@@ -1,1 +1,16 @@
 #include "Params.h"
+#include <iostream>
+#include <cstdlib>
+
+// Ověříme, že všechny parametry byly správně nastaveny
+bool Params::isValid() {
+    if (interfaceName.empty()) {
+        std::cerr << "ERROR: Network interface is not specified." << std::endl;
+        return false;
+    }
+    if (host.empty()) {
+        std::cerr << "ERROR: Hostname or IP address is not specified." << std::endl;
+        return false;
+    }
+    return true;
+}
