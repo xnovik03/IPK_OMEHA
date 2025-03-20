0535c3d66eaf417a38ad103b2f8416c52f5a97fb
xnovik03 <xnovik03@vutbr.cz>
2025-03-20 15:52:06 +0100

    add processing of sending packages

--- UDPscanner.cpp
+++ UDPscanner.cpp
@@ -7,37 +7,15 @@
 #include <unistd.h>
 #include <netdb.h>
 #include "UDPscanner.h"
+#include "Packetsender.h" 
 
 void UDPScanner::scanPort(const std::string &ip, int port) {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        std::cout << "Socket creation failed." << std::endl;
-        return;
-    }
+    // Místo přímého odesílání UDP paketu voláme Packetsender
+    bool result = Packetsender::sendUdpPacket(ip, port);  // Odeslání UDP paketu
 
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(ip.c_str());
-
-    // Odeslat UDP paket na port
-    char msg[] = "Test";
-    int result = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&server, sizeof(server));
-    if (result == -1) {
-        std::cout << "Error sending UDP packet." << std::endl;
-    }
-
-    // Čekání na odpověď
-    struct sockaddr_in from;
-    socklen_t fromlen = sizeof(from);
-    char buffer[512];
-    int bytesReceived = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&from, &fromlen);
-
-    if (bytesReceived == -1) {
-        std::cout << ip << " " << port << " udp closed" << std::endl;
-    } else {
+    if (result) {
         std::cout << ip << " " << port << " udp open" << std::endl;
+    } else {
+        std::cout << ip << " " << port << " udp closed" << std::endl;
     }
-
-    close(sockfd);
 }
