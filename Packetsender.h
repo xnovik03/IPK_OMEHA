af87737b278383d342cd9842e08a9d3453119726
xnovik03 <xnovik03@vutbr.cz>
2025-03-21 10:04:47 +0100

    fix sender package for tcp

--- Packetsender.h
+++ Packetsender.h
@@ -8,6 +8,9 @@ public:
     // Metoda pro odeslání SYN paketu pro TCP skenování
     static bool sendTcpPacket(const std::string &ip, int port);
 
+     // Výpočet checksumu pro IP a TCP hlavičky
+    static unsigned short checksum(unsigned short *buf, int len);
+
     // Metoda pro odeslání UDP paketu
     static bool sendUdpPacket(const std::string &ip, int port);
 };
