87779c6acaf79ab9932e35e2c80720383d156030
xnovik03 <xnovik03@vutbr.cz>
2025-03-21 11:00:56 +0100

    fix for ipv6

--- Packetsender.cpp
+++ Packetsender.cpp
@@ -9,59 +9,81 @@
 #include <netinet/tcp.h>   // For TCP header (tcphdr and flags like TH_SYN)
 
 bool Packetsender::sendTcpPacket(const std::string &ip, int port) {
-    // Kontrola, zda je IP adresa platná
-    if (ip.empty() || inet_addr(ip.c_str()) == INADDR_NONE) {
-        std::cerr << "Invalid IP address: " << ip << std::endl;
-        return false;
-    }
-
-    // Vytvoření socketu pro TCP
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Socket creation failed for TCP" << std::endl;
-        return false;
-    }
-
+    int sockfd;
     struct sockaddr_in dest;
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(port);
-    dest.sin_addr.s_addr = inet_addr(ip.c_str());  // Použití zadané IP adresy
+    struct sockaddr_in6 dest6;
 
-    // Pokus o připojení na TCP port
-    if (connect(sockfd, (struct sockaddr*)&dest, sizeof(dest)) < 0) {
-        std::cerr << "Connection failed to " << ip << ":" << port << std::endl;
-        close(sockfd);
-        return false;
+    if (ip.find(":") != std::string::npos) {  // Kontrola pro IPv6 adresy
+        sockfd = socket(AF_INET6, SOCK_STREAM, 0);  // IPv6 socket
+        if (sockfd == -1) {
+            std::cerr << "Socket creation failed for IPv6" << std::endl;
+            return false;
+        }
+        dest6.sin6_family = AF_INET6;
+        dest6.sin6_port = htons(port);
+        inet_pton(AF_INET6, ip.c_str(), &dest6.sin6_addr);
+      
+        if (connect(sockfd, (struct sockaddr *)&dest6, sizeof(dest6)) == 0) {
+            close(sockfd);
+            return true;
+        }
+    } else {  // IPv4
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            std::cerr << "Socket creation failed for IPv4" << std::endl;
+            return false;
+        }
+        dest.sin_family = AF_INET;
+        dest.sin_port = htons(port);
+        inet_pton(AF_INET, ip.c_str(), &dest.sin_addr);
+        if (connect(sockfd, (struct sockaddr *)&dest, sizeof(dest)) == 0) {
+            close(sockfd);
+            return true;
+        }
     }
 
-    std::cout << "TCP connection successful to " << ip << ":" << port << std::endl;
+    std::cerr << "Failed to connect to " << ip << ":" << port << std::endl;
     close(sockfd);
-    return true;
+    return false;
 }
 
 
-// Implementace funkce pro odeslání UDP paketu
 bool Packetsender::sendUdpPacket(const std::string &ip, int port) {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Socket creation failed for UDP" << std::endl;
-        return false;
-    }
-
+    int sockfd;
     struct sockaddr_in dest;
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(port);
-    dest.sin_addr.s_addr = inet_addr(ip.c_str());
+    struct sockaddr_in6 dest6;
 
-    // Odeslání UDP paketu
-    char message[] = "Test";
-    if (sendto(sockfd, message, sizeof(message), 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
-        std::cerr << "Error sending UDP packet to " << ip << ":" << port << std::endl;
-        close(sockfd);
-        return false;
+    if (ip.find(":") != std::string::npos) {  // IPv6
+        sockfd = socket(AF_INET6, SOCK_DGRAM, 0);  // IPv6 socket
+        if (sockfd == -1) {
+            std::cerr << "Socket creation failed for IPv6" << std::endl;
+            return false;
+        }
+        dest6.sin6_family = AF_INET6;
+        dest6.sin6_port = htons(port);
+        inet_pton(AF_INET6, ip.c_str(), &dest6.sin6_addr);
+        // Odeslani UDP paketu na IPv6
+        if (sendto(sockfd, "Test", 4, 0, (struct sockaddr *)&dest6, sizeof(dest6)) < 0) {
+            std::cerr << "Error sending UDP packet to " << ip << ":" << port << std::endl;
+            close(sockfd);
+            return false;
+        }
+    } else {  // IPv4
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sockfd == -1) {
+            std::cerr << "Socket creation failed for IPv4" << std::endl;
+            return false;
+        }
+        dest.sin_family = AF_INET;
+        dest.sin_port = htons(port);
+        inet_pton(AF_INET, ip.c_str(), &dest.sin_addr);
+        if (sendto(sockfd, "Test", 4, 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
+            std::cerr << "Error sending UDP packet to " << ip << ":" << port << std::endl;
+            close(sockfd);
+            return false;
+        }
     }
 
-    std::cout << "UDP packet sent to " << ip << ":" << port << std::endl;
     close(sockfd);
     return true;
 }
