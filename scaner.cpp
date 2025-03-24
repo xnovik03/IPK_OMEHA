711de17d2754ed8e46b82da661b8b65d187ad8d2
xnovik03 <xnovik03@vutbr.cz>
2025-03-24 11:42:05 +0100

    clean code

--- scaner.cpp
+++ scaner.cpp
@@ -8,31 +8,7 @@
 
 using namespace std;
 
-void scaner::parseArguments(int argc, char* argv[]) {
-    string interface, tcpPorts, udpPorts, host;
-    int timeout = 5000;
 
-    // Zpracování argumentů
-    for (int i = 1; i < argc; i++) {
-        string arg = argv[i];
-
-        if (arg == "-i" || arg == "--interface") {
-            if (i + 1 < argc) interface = argv[++i];
-        }
-        else if (arg == "-t" || arg == "--pt") {
-            if (i + 1 < argc) tcpPorts = argv[++i];
-        }
-        else if (arg == "-u" || arg == "--pu") {
-            if (i + 1 < argc) udpPorts = argv[++i];
-        }
-        else if (arg == "-w" || arg == "--wait") {
-            if (i + 1 < argc) timeout = stoi(argv[++i]);
-        }
-        else {
-            // Hostname nebo IP adresa je poslední argument
-            host = argv[i];
-        }
-    }
 
     // Zajištění, že byly zadány všechny parametry
     if (interface.empty() || tcpPorts.empty() || udpPorts.empty() || host.empty()) {
