fd7f03506836a04ebfa742d1cd99b8139b2d06e0
xnovik03 <xnovik03@vutbr.cz>
2025-03-24 11:56:45 +0100

    add comments

--- UDPscanner.cpp
+++ UDPscanner.cpp
@@ -11,9 +11,11 @@
 #include "Packetsender.h" 
 #include "ResultFormatter.h"
 
+// Funkce scanPort provádí skenování UDP portu na zadané IP adrese.
 void UDPScanner::scanPort(const std::string &ip, int port) {
+    // Získáme stav portu (OPEN nebo CLOSED) podle přijaté ICMP odpovědi
     Packetsender::PortState state = Packetsender::sendUdpPacket(ip, port);
-
+    // Výpis výsledku
     if (state == Packetsender::OPEN) {
         ResultFormatter::formatUdpResult(ip, port, "open");
     } else {  // CLOSED
