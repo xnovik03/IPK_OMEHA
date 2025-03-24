b31ff5d5ed33f703082fb96f4af227de3685caf9
xnovik03 <xnovik03@vutbr.cz>
2025-03-24 11:53:58 +0100

    add comments

--- TCPscanner.cpp
+++ TCPscanner.cpp
@@ -11,9 +11,11 @@
 #include "Packetsender.h"
 #include "ResultFormatter.h" 
 
+// Funkce scanPort provádí skenování daného TCP portu na zadané IP adrese.
 void TCPScanner::scanPort(const std::string &ip, int port) {
+    // Získáme stav portu (OPEN, CLOSED, FILTERED) pomocí TCP connect techniky
     Packetsender::PortState state = Packetsender::sendTcpPacket(ip, port);
-
+    // Výstup podle stavu portu
     if (state == Packetsender::OPEN) {
         ResultFormatter::formatTcpResult(ip, port, "open");
     } else if (state == Packetsender::CLOSED) {
